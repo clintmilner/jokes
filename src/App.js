d9a58fe9b119df28a1ffff55876f72db703ae0ba
Clint Milner <cmilner@datto.com>
2021-04-03 16:24:55 +0100

    gradients and better text styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import Joke from "./Joke";
+import {useState} from "react";
+import gradients from './gradients.json'
+
 
 const App = () => {
+    const [joke, setJoke] = useState('Tap for a joke');
+    const [loading, setLoading] = useState(true);
+    const [bg, setBg] = useState(gradients[1])
+
+    const loadJoke = async () => {
+    const randNum = Math.floor(Math.random() * gradients?.length) + 1
+        setLoading(true)
+        setBg(gradients[randNum])
+        const URL = 'https://icanhazdadjoke.com/'
+        const headers = {
+            'Accept': 'application/json'
+        }
+        const response = await fetch(URL, {headers})
+        const data = await response.json()
+
+        setJoke(data?.joke)
+        setLoading(false)
+    }
+
   return (
-    <div className="App">
-        <Joke />
+    <div className="App" style={{
+        backgroundImage: `linear-gradient(60deg, ${bg.colors.toString()})`
+    }}>
+        <Joke joke={joke} loading={loading} onClick={loadJoke} />
     </div>
   );
 }
--- src/Joke.js
+++ src/Joke.js
@@ -1,31 +1,17 @@
-import {useEffect, useState} from 'react'
+import {useEffect} from 'react'
 import logo from './logo.svg';
 
-const Joke = () => {
-    const [joke, setJoke] = useState('Tap for a joke');
-    const [loading, setLoading] = useState(true);
+const Joke = ({joke, loading, onClick}) => {
 
-    const loadJoke = async () => {
-        setLoading(true)
-        const URL = 'https://icanhazdadjoke.com/'
-        const headers = {
-            'Accept': 'application/json'
-        }
-        const response = await fetch(URL, {headers})
-        const data = await response.json()
-
-        setJoke(data?.joke)
-        setLoading(false)
-    }
 
     useEffect(() => {
-        loadJoke()
+        onClick()
     }, [])
 
 
     return <div className="joke">
         {loading ? <img width={100} src={logo} className="loading" alt="loading"/> : <p>{joke}</p>}
-        <button onClick={loadJoke}>tell me another one</button>
+        <button onClick={onClick}>tell me another one</button>
     </div>
 }
 
